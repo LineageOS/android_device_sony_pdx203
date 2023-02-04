2b5e342513476f856327d7569878ce87d2884125
Luofan Chen <clfbbn@gmail.com>
2023-02-04 20:34:28 +0800

    pdx203: Decommonize device key overlay
    pdx203 does not have assistant key while pdx206 has
    
    Change-Id: I24fb43116e02721e760087d660805a9c0e4b8914

--- device.mk
+++ device.mk
@@ -38,6 +38,9 @@ PRODUCT_PACKAGES += \
     android.hidl.manager@1.0
 
 # Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage
+
 PRODUCT_PACKAGES += \
    SonyPDX203SystemUIRes 
 
